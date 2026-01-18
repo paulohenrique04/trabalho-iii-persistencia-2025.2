5015f0fa894c623a13307b882384111713bf8865
JoaoAlves-Web <joaoalves.quix10@gmail.com>
2026-01-18 16:51:09 -0300

    Refactor User model and add Pydantic schemas
    Refactored User model and added UserCreate and UserUpdate schemas.

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,28 @@
+
 from datetime import datetime
 from beanie import Document
-from pydantic import Field
-
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class User(Document):
     username: str
     email: str
     password: str
-    bio: str = None
-    birthdate: datetime = None
-    gender: str = None
-    country: str = None
-    telephone: str = None
-    city: str = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-
+    
+    
     class Settings:
         name = "users"
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+    bio: Optional[str] = None
+    birthdate: Optional[datetime] = None
+    
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    
+
